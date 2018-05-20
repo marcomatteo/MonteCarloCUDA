b93279873447279e63fe8237ef2bf040f4fa6939
MarcoMatteoBuzzulini <buzh92@gmail.com>
2018-05-20 18:53:29 +0200

    commento

--- cvaEquityOption/MonteCarlo.h
+++ cvaEquityOption/MonteCarlo.h
@@ -39,7 +39,7 @@ __host__ void cuda_error_check(const char * prefix, const char * postfix){
 	if (cudaPeekAtLastError() != cudaSuccess){
 		printf("\n%s%s%s", prefix, cudaGetErrorString(cudaGetLastError()), postfix);
 		cudaDeviceReset();
-		wait_exit();
+		//wait_exit();
 		exit(1);
 	}
 }
