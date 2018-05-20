0918652f7b59452d79a2767036e6d943d96e0451
MarcoMatteoBuzzulini <buzh92@gmail.com>
2018-05-20 18:05:40 +0200

    cudaGetLastError update

--- cvaEquityOption/MonteCarlo.h
+++ cvaEquityOption/MonteCarlo.h
@@ -32,6 +32,23 @@
 	} }
 #endif
 
+// This will output the proper error string when calling cudaGetLastError
+#define getLastCudaError(msg)      __getLastCudaError (msg, __FILE__, __LINE__)
+
+inline void __getLastCudaError(const char *errorMessage, const char *file, const int line)
+{
+    cudaError_t err = cudaGetLastError();
+
+    if (cudaSuccess != err)
+    {
+        fprintf(stderr, "%s(%i) : getLastCudaError() CUDA error : %s : (%d) %s.\n",
+                file, line, errorMessage, (int)err, cudaGetErrorString(err));
+        DEVICE_RESET
+        exit(EXIT_FAILURE);
+    }
+}
+#endif
+
 typedef struct {
     double s;    // stock price
     double k;    // strike price
