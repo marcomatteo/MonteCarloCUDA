17b7343f9ed86fb25ab141c27b6cd1379868eb64
MarcoMatteoBuzzulini <buzh92@gmail.com>
2018-05-30 11:48:33 +0200

    dimenticanza

--- MonteCarloGPUpricing/MonteCarlo.h
+++ MonteCarloGPUpricing/MonteCarlo.h
@@ -98,7 +98,6 @@ typedef struct{
 
 //	Host functions declarations
 void Chol( double c[N][N], double a[N][N] );
-double host_bsCall ( OptionData );
 OptionValue host_vanillaOpt(OptionData, int);
 OptionValue host_basketOpt(MultiOptionData*, int);
 void host_cvaEquityOption(CVA *cva, int numBlocks, int numThreads);
