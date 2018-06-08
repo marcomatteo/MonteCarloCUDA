58ab3e7423f072223971e7c09825470ec43c07cd
MarcoMatteoBuzzulini <buzh92@gmail.com>
2018-06-08 22:55:32 +0200

    rimetto

--- MonteCarloGPUpricing/vanillaOpt.cu
+++ MonteCarloGPUpricing/vanillaOpt.cu
@@ -100,3 +100,59 @@ int main(int argc, const char * argv[]) {
     CudaCheck( cudaEventDestroy( d_stop ));
     return 0;
 }
+///////////////////////////////////
+//    ADJUST FUNCTIONS
+///////////////////////////////////
+
+void sizeAdjust(cudaDeviceProp *deviceProp, int *numBlocks, int *numThreads){
+    int maxGridSize = deviceProp->maxGridSize[0];
+    int maxBlockSize = deviceProp->maxThreadsPerBlock;
+    //    Replacing in case of wrong size
+    if(*numBlocks > maxGridSize){
+        *numBlocks = maxGridSize;
+        printf("Warning: maximum size of Grid is %d",*numBlocks);
+    }
+    if(*numThreads > maxBlockSize){
+        *numThreads = maxBlockSize;
+        printf("Warning: maximum size of Blocks is %d",*numThreads);
+    }
+}
+
+void memAdjust(cudaDeviceProp *deviceProp, int *numThreads){
+    size_t maxShared = deviceProp->sharedMemPerBlock;
+    size_t maxConstant = deviceProp->totalConstMem;
+    int sizeDouble = sizeof(double);
+    int numShared = sizeDouble * *numThreads * 2;
+    if(sizeof(MultiOptionData) > maxConstant){
+        printf("\nWarning: Excess use of constant memory: %zu\n",maxConstant);
+        printf("A double variable size is: %d\n",sizeDouble);
+        printf("In a MultiOptionData struct there's a consumption of %zu constant memory\n",sizeof(MultiOptionData));
+        printf("In this Basket Option there's %d stocks\n",N);
+        int maxDim = (int)maxConstant/(sizeDouble*5);
+        printf("The optimal number of dims should be: %d stocks\n",maxDim);
+    }
+    if(numShared > maxShared){
+        printf("\nWarning: Excess use of shared memory: %zu\n",maxShared);
+        printf("A double variable size is: %d\n",sizeDouble);
+        int maxThreads = (int)maxShared / (2*sizeDouble);
+        printf("The optimal number of thread should be: %d\n",maxThreads);
+    }
+    printf("\n");
+}
+
+extern "C" void Parameters(int *numBlocks, int *numThreads){
+    cudaDeviceProp deviceProp;
+    int i = 0;
+    CudaCheck(cudaGetDeviceProperties(&deviceProp, 0));
+    numThreads[0] = 128;
+    numThreads[1] = 256;
+    numThreads[2] = 512;
+    numThreads[3] = 1024;
+    printf("\nParametri CUDA:\n");
+    printf("Scegli il numero di Blocchi: ");
+    scanf("%d",numBlocks);
+    for (i=0; i<THREADS; i++) {
+        sizeAdjust(&deviceProp,numBlocks, &numThreads[i]);
+        memAdjust(&deviceProp, &numThreads[i]);
+    }
+}
