f33905a1cf0d251f0a25d506f5e2b244e9dcad1f
MarcoMatteoBuzzulini <buzh92@gmail.com>
2018-06-26 11:27:02 +0200

    annulatoper sbaglio

--- tesi_originale/double_precision/MonteCarloKernel.cu
+++ tesi_originale/double_precision/MonteCarloKernel.cu
@@ -305,7 +305,7 @@ void MonteCarlo(dev_MonteCarloData *data){
         option.v[0] = data->sopt.v;
         option.k = data->sopt.k;
         option.r = data->sopt.r;
-        option.t = data->sopt->t;
+        option.t = data->sopt.t;
         r = data->sopt.r;
         t = data->sopt.t;
         int n_option = 1;
