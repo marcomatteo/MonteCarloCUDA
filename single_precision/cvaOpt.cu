1ed27c8029377d32faa33634dc85507a32c8e3a8
MarcoMatteoBuzzulini <buzh92@gmail.com>
2018-06-29 22:09:06 +0200

    poi

--- single_precision/cvaOpt.cu
+++ single_precision/cvaOpt.cu
@@ -74,7 +74,7 @@ int main(int argc, const char * argv[]) {
     CudaCheck( cudaEventSynchronize( d_stop ));
     CudaCheck( cudaEventElapsedTime( &CPU_timeSpent, d_start, d_stop ));
     
-    printf("\nCVA: \n%f \n\n",host_result.Expected, host_result.Confidence);
+    printf("\nCVA: \n%f \n",host_result.Expected, host_result.Confidence);
     printf("\nTotal execution time: (ms)\n%f\n\n", CPU_timeSpent);
     printf("--------------------------------------------------\n");
     
